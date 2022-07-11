1c3a44430db8a47fa1d69e6267b03c25df4a673d
rkhanna <rkhanna@chegg.com>
2022-07-11 14:58:54 +0530

    vercel build fix

--- src/main/UserForm.js
+++ src/main/UserForm.js
@@ -1,8 +1,8 @@
 import React from "react";
-import useForm from "./hooks/useForm";
-import { userForm } from "./formConfig";
+import useForm from "../hooks/useForm";
+import { userForm } from "../formConfig";
 
-import "./styles.css";
+import "../styles.css";
 import "./UserForm.css";
 
 export default function UserForm() {
